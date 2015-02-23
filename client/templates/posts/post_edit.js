6ab500ba7badd6d7f39451b44b9d6ba7969bb3bf
Marco Vintenove <marcovintenove@gmail.com>
2015-02-23 21:56:18 +0800

    Add edit posts functionality

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,28 @@
+Template.editPage.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var currentPostId = this._id;
+
+    var postProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    Meteor.update(currentPostId, {$set: postProperties}, function (error) {
+      if (error)
+        return alert(error.reason);
+      else
+        Router.go('postPage', {_id : currentPostId});
+    });
+  },
+
+  'click .delete': function(e) {
+    e.preventDefault();
+
+    if (confirm("Delete this post?")) {
+      Posts.remove(this._id);
+      Router.go('postList');
+    }
+  }
+});
