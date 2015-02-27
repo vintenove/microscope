f9cb0a3868ce6791ae78a673eb3f79c8212952a8
Marco Vintenove <marcovintenove@gmail.com>
2015-02-27 23:32:14 +0800

    Fix typos, go to sleep!

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -1,12 +1,11 @@
 Template.errors.helpers({
   errors: function(){
-    console.log(Errors.find());
     return Errors.find();
   }
 });
 
-Template.errors.rendered = function(){
-  var error = this.data();
+Template.error.rendered = function(){
+  var error = this.data;
   Meteor.setTimeout(function() {
     Errors.remove(error._id);
   }, 3000);
