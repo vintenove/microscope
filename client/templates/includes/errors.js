ab8b0d03f3ca7b988f8752b7810a32a104c027cd
Marco Vintenove <marcovintenove@gmail.com>
2015-02-27 23:13:25 +0800

    Clears error after 3000 millis

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -1,5 +1,13 @@
 Template.errors.helpers({
   errors: function(){
-    return Errors.find();  
+    console.log(Errors.find());
+    return Errors.find();
   }
 });
+
+Template.errors.rendered = function(){
+  var error = this.data();
+  Meteor.setTimeout(function() {
+    Errors.remove(error._id);
+  }, 3000);
+};
