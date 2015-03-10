62cb9f758aeb312492ff98951a16aa48a505fc65
Marco Vintenove <marcovintenove@gmail.com>
2015-03-10 23:52:34 +0800

    Fix notifications subscriber bug

--- server/publications.js
+++ server/publications.js
@@ -7,6 +7,6 @@ Meteor.publish('comments', function(postId) {
   return Comments.find({postId : postId});
 });
 
-Meteor.publish('notifications', function(userId) {
-  return Notifications.find({userId : userId});
+Meteor.publish('notifications', function() {
+  return Notifications.find();
 });
